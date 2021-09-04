12826840ee953cc78232d1eebdd7109b75602af5
J Edwards <jesse.edwards2@protonmail.com>
2021-09-04 11:20:17 -0400

    created plane component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Canvas} from '@react-three/fiber'
 import Box from './components/Box'
 import Controls from './components/Controls'
+import Plane from './components/Plane'
 const App = () => {
 
   
@@ -9,6 +10,7 @@ const App = () => {
     <Canvas>
       <Controls/>
       <Box/>
+      <Plane/>
     </Canvas>
   )
 }
